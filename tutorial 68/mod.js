37408cb7cf2cf42575e9de90747184b30dd8e832
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-04-27 17:23:35 +0600

    came back

---
+++ tutorial 68/mod.js
@@ -0,0 +1,9 @@
+console.log('this is mod')
+function sum(first, second){
+    let sum = (first + second);
+    console.log(sum)
+    return sum;
+};
+// sum(3,4);
+
+module.exports = sum;
---
+++ tutorial 68/script.js
@@ -0,0 +1,2 @@
+const sum = require("./mod");
+console.log(sum(3,7));
