682fcbb6292170731373b2bf039bc1e4674e022e
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-04-29 17:46:07 +0600

    tutorial 72 finished

--- tutorial70#express/script.js
+++ tutorial70#express/script.js
@@ -1,7 +1,18 @@
 const express = require('express')
-
+const path = require('path')
 const app = express();
 const port = 3000;
+
+app.use('/static', express.static('static'))
+app.set('view engine', 'pug')
+
+app.set('views', path.join(__dirname, 'views'));
+
+//using pug 
+app.get('/demo', (red, res)=>{
+    res.status(200).render('demo', { title: 'shajidur rahman', message: 'Hello there! i am shajidur rahman' })
+})
+
 
 app.get('/', (red, res)=>{
     res.send('this is my first app with express')
@@ -14,5 +25,5 @@ app.get('/contuct', (red, res)=>{
 })
 
 app.listen(port, ()=>{
-    console.log(`the port is started at ${port}`);
+    console.log(`the port is started at http://localhost:${port}`);
 })
