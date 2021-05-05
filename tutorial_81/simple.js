c530c06f234d510db9bd2b456fbd6f41d8bebb90
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-05-05 17:36:33 +0600

    tutorial 82 finished

---
+++ tutorial_81/simple.js
@@ -0,0 +1,3 @@
+// inserting data in mongodb
+db.items.insertMany({ name: "i phone", price: 222, rattings: 4.9, q: 222, sold: 30 })
+db.items.insertMany({ name: "i phone", price: 222, rattings: 4.9, q: 222, sold: 30 })
