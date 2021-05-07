435eec6667fd8e6425d558558b8c38a02b1b4a69
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-05-07 16:32:15 +0600

    tutorial85 finished

---
+++ tutorial85/index.js
@@ -0,0 +1,9 @@
+// getting-started.js
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/shajidur', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('hi, it is mongoose')
+});
