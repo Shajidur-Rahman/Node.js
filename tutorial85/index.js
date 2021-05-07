b4a2fcd380fcc46d8f83b9b8bc406ee94308c9d7
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-05-07 17:31:30 +0600

    tutorial 86 finished

--- tutorial85/index.js
+++ tutorial85/index.js
@@ -4,6 +4,34 @@ mongoose.connect('mongodb://localhost/shajidur', { useNewUrlParser: true, useUni
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', function () {
     console.log('hi, it is mongoose')
 });
+
+const kittySchema = new mongoose.Schema({
+    name: String
+});
+
+
+// NOTE: methods must be added to the schema before compiling it with mongoose.model()
+kittySchema.methods.speak = function () {
+    const greeting = "My name is " + this.name
+    // console.log(greeting);
+} 
+const testing = mongoose.model('testing', kittySchema); // here will creat collections 
+
+const shajidur = new testing({ name: 'shajidur rahman' });
+console.log(shajidur.name); // 'Silence'
+
+const jannat = new testing({ name: 'shahanaj' }); // here will added objects to the collections
+console.log(jannat.name); // 'Silence'
+
+shajidur.save(function (err, shajidur) { // it will save all the collections to the db
+    if (err) return console.error(err);
+    shajidur.speak();
+});
+
+testing.find({name:"shajidur rahman"},function (err, kittens) {
+    if (err) return console.error(err);
+    console.log(kittens);
+})
