4e6e50abd95c206a888cd9842764589e82a1366b
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-05-08 14:05:05 +0600

    tutorial 88 finished

--- tutorial_73/script.js
+++ tutorial_73/script.js
@@ -1,8 +1,21 @@
 const port = process.env.PORT || 3000;
 const http = require('http');
 const express = require('express');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/contuctdance', { useNewUrlParser: true, useUnifiedTopology: true });
 const app = express();
+const bodyparser = require('body-parser')
 const fs = require('fs')
+// mongodb
+const contuctSchema = new mongoose.Schema({
+    name: String,
+    age: String,
+    email: String,
+    gender: String,
+    about: String
+});
+
+const contuct = mongoose.model('contuct', contuctSchema); // here will creat collections 
 
 // EXPRESS CONFIGARETION
 app.use('/static', express.static('static'))
@@ -12,25 +25,32 @@ app.use(express.urlencoded())
 
 // ENDING POINT
 
-const send = {'title':'this is written by pug ', 'contant':'lorem34'};
-app.get('/', (red, res)=>{
+const send = { 'title': 'this is written by pug ', 'contant': 'lorem34' };
+app.get('/', (red, res) => {
     res.status(200).render('index.pug', send);
 })
 
-app.post('/', (req, res)=>{
-    let name = req.body.name;
-    let age = req.body.age;
-    let email = req.body.email;
-    let gender = req.body.gender;
-    let about = req.body.about;
-    let output = `the client is ${name}, ${age} years old, email is ${email}, gender is ${gender}, and his about is ${about}`
-    fs.writeFileSync('output.txt', output)
-    const send = {'message':'your form has been submitted successfully'};
-    res.status(200).render('index.pug', send);
-    
+app.post('/', (req, res) => {
+    // let name = req.body.name;
+    // let age = req.body.age;
+    // let email = req.body.email;
+    // let gender = req.body.gender;
+    // let about = req.body.about;
+    // let output = `the client is ${name}, ${age} years old, email is ${email}, gender is ${gender}, and his about is ${about}`
+    // fs.writeFileSync('output.txt', output)
+    // const send = { 'message': 'your form has been submitted successfully' };
+    // res.status(200).render('index.pug', send);
+    let mydata = new contuct(req.body);
+    mydata.save().then(()=>{
+       res.send('successfully saved');
+    }).catch(()=>{
+        res.status(400).send('items is not saved')
+    })
+
+
 })
 
 // STARTING THE SERVER 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`the port is started at http://localhost:${port}`);
 })
